1a5adb8c1828e8b09334cabd341abdd0bfd95085
verdanceluke <luke@verdance.co>
2023-04-20 15:12:25 -0700

    test predict endpoint

--- model_prediction_server/tests/test_endpoints.py
+++ model_prediction_server/tests/test_endpoints.py
@@ -7,4 +7,15 @@ def test_status_endpoint(client: TestClient):
     assert json_contents == "works"
 
 def test_predict(client: TestClient):
-    pass
+    json_post_dict = {
+        "sepal_length": 5,
+        "sepal_width": 6,
+        "petal_length": 7,
+        "petal_width": 8
+    }
+
+    response = client.post(
+        '/predict',
+        json=json_post_dict
+    )
+    assert response.status_code == 201
