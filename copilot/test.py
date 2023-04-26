70012267fae2b5fdecec2b9e69d6bcd364e9c55a
verdanceluke <luke@verdance.co>
2023-04-26 08:18:47 -0700

    copilot is cool

--- copilot/test.py
+++ copilot/test.py
@@ -1,6 +1,13 @@
-# write a function that parses a file and outputs the number of asterisks
+# write a function that parses a .csv and returns the number of asterisk characters in the file
 
+import csv
 
-def parse_file(filename):
-    pass
+def count_asterisks(filename):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        count = 0
+        for row in reader:
+            count += row.count('*')
+        return count
+
 
